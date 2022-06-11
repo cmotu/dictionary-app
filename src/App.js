c9f7bffb3461b22a2787adfd956525e94ca71209
cmotu <cmotu@sandiego.edu>
2022-06-11 11:53:28 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 
-import './App.css';
 import Dictionary from "./Dictionary";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-      
-      
-        <Dictionary />
-      
-    </div>
+      <div className="container">
+        <header className="App-header">
+          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+            <img src={logo} className="logo" alt="SheCodes Logo" />
+          </a>
+        </header>
+        <Dictionary defaultKeyword="sunset" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           Christina Motu
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/cmotu/dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://cozy-zuccutto-9a091a.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
