f0bc3f81b6ed1f94460bba1be1e6b7ba270227b6
cmotu <cmotu@sandiego.edu>
2022-06-13 07:46:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        
+        <header>
+          <span>Dictionary App</span>
+        </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunflower"/>
         </main>
         <footer className="App-footer">
           <small>Coded by Christina Motu</small>
