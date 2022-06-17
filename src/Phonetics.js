92fb3c0d5f658156dc277cee304cf0fc3979e703
cmotu <cmotu@sandiego.edu>
2022-06-17 14:07:07 -0700

    Update Phonetics.js

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,10 +4,10 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      <span className="text">{props.phonetic.text}</span>
+      <span className="text">{props.phonetics.text}</span>
     </div>
   );
 }
