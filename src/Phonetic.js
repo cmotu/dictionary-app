34eeb19f4e4454652ded257983c88c5e5fe987b3
cmotu <cmotu@sandiego.edu>
2022-06-17 12:47:31 -0700

    added styling

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,20 +2,12 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  if (props.phonetic) {
-    return (
-      <div className="Phonetic">
-        <a
-          href={props.phonetic.audio}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Listen
-        </a>
-        <h2>{props.phonetic.text}</h2>
-      </div>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>
+      <span className="text">{props.phonetic.text}</span>
+    </div>
+  );
 }
